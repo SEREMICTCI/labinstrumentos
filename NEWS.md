002e0f525dae41895534350e8a428bd7c3ddbc55
matcasti <matcasti@umag.cl>
2021-11-14 00:22:09 -0300

    v 0.0.0.9001

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,9 @@
+# labinstrumentos 0.0.0.9001
+
+* Se incluyó argumento `vertex.cex` en la función `grafico_red_circular()` para personalizar el tamaño de los nodos.
+* Se eliminó el argumento `main` de la función `grafico_red_hubs()`.
+* Se mejora el proceso de limpieza de los datos ([ver script](https://github.com/SEREMICTCI/labinstrumentos/blob/master/data-raw/lab_instrumentos.R)) utilizando funciones vectorizadas para mejorar los tiempos de computación. Del mismo modo se notan mejoras en el resultado final, eliminando stopWords que antes no se podían eliminar con facilidad.
+
 # labinstrumentos 0.0.0.9000
 
 * Se mejora la estética del sitio web inicial.
--- data-raw/lab_instrumentos.R
+++ data-raw/lab_instrumentos.R
@@ -1,51 +1,89 @@
 
 # Cargamos paquetes ---------------------------------------------------------------------------
 
-library(data.table)
-library(googlesheets4)
+
+library(googlesheets4) # Descargar datos de google sheets
+library(data.table)    # Manipulación de variables
+library(stringi)       # Reemplazo de stopWords (limpieza de texto)
+
 
 # Importamos los datos ------------------------------------------------------------------------
 
+
+## Cargamos los datos directamente de Google Sheets
 lab_instrumentos <- googlesheets4::read_sheet(
   ss = "1ct6zRIDli4AHjk8QsF4CYSwZzzTVGK0ncCDBXtXhmqY",
   sheet = "Lista P-C-E",
 )
 
+## Cargamos stopWords usadas para el proceso de limpieza posterior
 stopWords <- readLines("data-raw/stopwords.txt")
+
+## Transformamos stopWords para identificación más precisa usando 'regex'
+stopWords <- paste0("(?<!\\S)\\b", stopWords, "\\b(?!\\S)")
+
 
 # Tratamiento previo --------------------------------------------------------------------------
 
+
+## Transformamos a data.table para mejor manipulación (i.e., sintaxis más concisa)
 lab_instrumentos <- data.table::as.data.table(lab_instrumentos)
 
+## Cambiamos nombres de columnas a minúsculas (mayor compatibilidad multi-plataforma)
 names(lab_instrumentos) <- c("laboratorio", "grupo", "problema", "cluster_problema_manual",
                             "clean_problema", "cluster_causa_manual", "causa",
                             "clean_causa", "consecuencia", "clean_consecuencia",
                             "region")
 
-# Tratamiento principal -----------------------------------------------------------------------
+## Creamos un objeto intermedio para asistirnos en el proceso de limpieza
+col_names <- names(lab_instrumentos)
 
-lab_instrumentos[, names(lab_instrumentos) := lapply(.SD, tolower)]
 
-lab_instrumentos[, names(lab_instrumentos) := lapply(.SD, gsub, pattern = "\\.", replacement = "")]
+# Estandarización de texto --------------------------------------------------------------------
 
-lab_instrumentos[, names(lab_instrumentos) := lapply(.SD, gsub, pattern = "\\,", replacement = "")]
 
+## Texto a minuscula - estandarización
+lab_instrumentos[, (col_names) := lapply(.SD, tolower)]
+
+## Se eliminan puntos
+lab_instrumentos[, (col_names) := lapply(.SD, gsub, pattern = "\\.", replacement = "")]
+
+## Se eliminan comas
+lab_instrumentos[, (col_names) := lapply(.SD, gsub, pattern = "\\,", replacement = "")]
+
+
+# Variables selecionadas para mayor limpieza (i.e., para análisis) ----------------------------
+
+
+## Se seleccionan variables para procesamiento adicional
 vars <- c("clean_problema", "clean_causa", "clean_consecuencia")
 
-for (i in stopWords) {
-  i_bound <- paste0("(?<!\\S)\\b", i, "\\b(?!\\S)")
-  lab_instrumentos[, (vars) := lapply(.SD, gsub, pattern = i_bound, replacement = "", perl = T), .SDcols = vars]
-}
+## Con esas variables, se eliminan las 'stopWords', sin incluir aquellas que esten precedidas o antecedidas por un guión, i.e. palabra compuesta (e.g., 'no-consideran')
+lab_instrumentos[, (vars) := lapply(.SD, stringi::stri_replace_all_regex, pattern = stopWords, replacement = "", vectorize_all = FALSE), .SDcols = vars]
 
-lab_instrumentos[, names(lab_instrumentos) := lapply(.SD, gsub, pattern = "\\s+", replacement = " ")]
 
-lab_instrumentos[, names(lab_instrumentos) := lapply(.SD, trimws)][]
+# Procedimientos complementarios y/o menores --------------------------------------------------
 
-# Tratamiento final (detalles) ----------------------------------------------------------------
 
-## No necesario
+## Se eliminan espacios entre palabras
+lab_instrumentos[, (col_names) := lapply(.SD, gsub, pattern = "\\s+", replacement = " ")]
+
+## Y espacios en los extremos de las oraciones (i.e., debido a la eliminación de palabras)
+lab_instrumentos[, (col_names) := lapply(.SD, trimws)][]
+
+
+# Eliminación de objetos intermedios ----------------------------------------------------------
+
+
+## Objetos ya usados, y que no cumplen otras funciones
+remove(stopWords, vars, col_names)
+
 
 # Exportamos los datos ------------------------------------------------------------------------
 
+
+## Guardamos los datos en CSV (facilidad de importación en múltiples softwares)
 data.table::fwrite(lab_instrumentos, file = "data-raw/lab_instrumentos.csv")
+
+## Guardamos los datos en RDS (facilidad de importación en R, con todas sus propiedades)
 usethis::use_data(lab_instrumentos, overwrite = TRUE)
