5b85b01a9acb1478252776dcfe2d9b2c1b1322e3
matcasti <matcasti@umag.cl>
2021-11-19 00:59:02 -0300

    Cambio de dependencias
    Se eliminó las dependencias de `RWeka`, usando `NLP` en cambio. Ojalá esto tenga mejor compatibilidad multiplataforma y no de problemas en el R-CMD-check por Java.
    
    También se creó un script dedicado para `fun_tokenizer` para mantener la legibilidad del código

--- R/obtener_terminos.R
+++ R/obtener_terminos.R
@@ -6,20 +6,25 @@
 #' @param i vector de carácteres.
 #' @param n_words Número de palabras combinadas.
 #'
-#' @importFrom RWeka NGramTokenizer Weka_control
 #' @importFrom tm VCorpus VectorSource DocumentTermMatrix
 #' @importFrom data.table data.table
 #'
 #' @export
 
-get_terms <- function(i, n_words = 1) {
-  fun_tokenizer <- function(x) {
-    RWeka::NGramTokenizer(x = x, control = RWeka::Weka_control(min = n_words,
-                                                               max = n_words))
-  }
-  m <- tm::VCorpus(x = tm::VectorSource(x = unique(i)))
-  m <- tm::DocumentTermMatrix(x = m, control = list(tokenize = fun_tokenizer))
-  m <- sort(x = colSums(x = as.matrix(m)), decreasing = TRUE)
+obtener_terminos <- function(i, n_words = 1) {
+
+  fun <- function(j) { fun_tokenizer(j, n_words) }
+
+  i <- unique(i)
+
+  m <- tm::VectorSource(x = i)
+  m <- tm::VCorpus(x = m)
+  m <- tm::DocumentTermMatrix(x = m, control = list(tokenize = fun))
+  m <- as.matrix(m)
+  m <- colSums(x = m)
+  m <- sort(x = m, decreasing = TRUE)
+
   terms <- data.table::data.table(WORD = names(m), FREQ = m)
+
   return(terms)
 }
